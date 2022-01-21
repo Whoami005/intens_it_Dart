34455ab40debb5242fb200f5db035f0f632ca113
OutLaw371 <gadjimurad040203@gmail.com>
2022-01-21 19:14:47 +0300

    упрощенный конструктор и this

--- 19DEC.dart
+++ 19DEC.dart
@@ -3,14 +3,14 @@ void main(){
   one.info();
   Train moscow = Train.toMoscow(time: '2 утра', number: 0);
   moscow.info();
-  Train am9 = Train.am9(city: 'LA', number: 111);
-  am9.info();
+  Train am9 = Train.am9(city: 'LA', number: 111)..info();
+  // am9.info();
 }
 
 class Train{
-  String city = '';
-  String time = '';
-  int number = 0;
+  String city;
+  String time;
+  int number;
 
   Train({required this.city, required this.time, this.number = 1}){
     // this.city = city;
@@ -18,15 +18,15 @@ class Train{
     // this.number = number;
   }
 
-  Train.toMoscow({required this.time, required this.number}){
-    this.city = 'Москва';
+  Train.toMoscow({required this.time, required this.number, this.city = 'Москва'}){
+    // this.city = 'Москва';
     // this.time = time;
     // this.number = number;
   }
 
-  Train.am9({required this.city, required this.number}){
+  Train.am9({required this.city, required this.number, this.time = '9 утра'}){
     // this.city = city;
-    this.time = '9 утра';
+    // this.time = '9 утра';
     // this.number = number;
   }
 
--- 21DEC.dart
+++ 21DEC.dart
@@ -1,3 +1,34 @@
 void main(){
+  Train one = Train(city: 'New York', time: '9 вечера', number: 100);
+  print(one);
+  // one.info();
+  Train moscow = Train.toMoscow(time: '2 утра', number: 0);
+  // print(moscow);
+  print(moscow.toString());
+  // moscow.info();
+  Train am9 = Train.am9(city: 'LA', number: 111);
+  print(am9);
+}
+
+class Train {
+  String city;
+  String time;
+  int number;
+
+  Train({required this.city, required this.time, this.number = 1}) {}
+
+  Train.toMoscow(
+      {required this.time, required this.number, this.city = 'Москва'}){}
+
+  Train.am9({required this.city, required this.number, this.time = '9 утра'}){}
+
+  @override
+  String toString() {
+    return 'Вы направляетесь в город: $city. Номер Вашего поезда: $number. Выезд в $time';
+  }
+
+  // void info() {
+  //   print('Вы направляетесь в город: $city. Номер Вашего поезда: $number. Выезд в $time');
+  // }
 
 }
